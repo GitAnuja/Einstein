54f7dfaa2111eb64cfe9dd5616cdf18e2aa7e6d7
Anuja Manoharan <anujamanoharan@gmail.com>
2018-04-09 22:06:47 +0530

    form

--- einstein.js
+++ einstein.js
@@ -10,5 +10,45 @@ function getValues(){
 			row.append(cell);
 		}
 		document.getElementById("values").append(row);
+		document.getElementById("form").style.display = "";
+	}
+}
+
+function form(){
+	var r = document.getElementById("r").value;
+	var c = document.getElementById("c").value;
+	var arr = [];
+	var table = document.getElementById("values").children;
+	var inner = Math.ceil(c/3);
+	for(var i=0; i<r; i++){
+		var arr1 = [];
+		var tr = table[i].children;
+		for(var j=0; j<c; j++){
+			arr1.push(tr[j].children[0].value);
+		}
+		arr.push(arr1);
+	}
+	for(var i=0; i<r; i++){
+		var row = document.createElement("tr");
+		for(var j=0; j<c; j++){
+			var cell = document.createElement("td");
+			var table1 = document.createElement("table");
+			table1.border = "1";
+			table1.width = "100%";
+			for(var k=0; k<inner; k++){
+				var row1 = document.createElement("tr");
+				for(var l=0; l<3; l++){
+					var cell1 = document.createElement("td");
+					var value = arr[i][k*3+l];
+					value = value ? value : "";
+					cell1.innerText = value;
+					row1.append(cell1);
+				}
+				table1.append(row1);
+			}
+			cell.append(table1);
+			row.append(cell);
+		}
+		document.getElementById("formValues").append(row);
 	}
 }
